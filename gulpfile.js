4bc67f303dd22172865f307555afe7145a94c081
omarhisham7 <95759422+omarhisham7@users.noreply.github.com>
2021-12-16 11:27:05 +0200

    commit 4

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,19 @@ const sass = require('gulp-sass')(require('sass'));
 const prefix = require('gulp-autoprefixer');
 const minify = require('gulp-clean-css');
 const terser = require('gulp-terser');
-//const imagemin = require('gulp-imagemin');
-//const imagewebp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const imagewebp = require('gulp-webp');
+
+//copy html 
+function copyHtml() {
+    return src('*.html')
+        .pipe(dest('dist'))
+};
+
 
 //compile, prefix, and min scss
-
-//error "will compile only style and media .scss"
 function compilescss() {
-    return src('src/scss/*.scss')
+    return src('./src/scss/**/*.scss')
         .pipe(sass())
         .pipe(prefix('last 2 versions'))
         .pipe(minify())
@@ -19,22 +24,22 @@ function compilescss() {
 
 
 
-// //optimize and move images
-// function optimizeimg() {
-//     return src('src/images/*.{jpg,png}')
-//         .pipe(imagemin([
-//             imagemin.mozjpeg({ quality: 80, progressive: true }),
-//             imagemin.optipng({ optimizationLevel: 2 }),
-//         ]))
-//         .pipe(dest('dist/images'))
-// };
+//optimize and move images
+function optimizeimg() {
+    return src('src/images/*.{jpg,png}')
+        .pipe(imagemin([
+            imagemin.mozjpeg({ quality: 80, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 2 }),
+        ]))
+        .pipe(dest('dist/src/images'))
+};
 
-// //optimize and move images
-// function webpImage() {
-//     return src('dist/images/*.{jpg,png}')
-//         .pipe(imagewebp())
-//         .pipe(dest('dist/images'))
-// };
+//optimize and move images
+function webpImage() {
+    return src('dist/images/*.{jpg,png}')
+        .pipe(imagewebp())
+        .pipe(dest('dist/src/images'))
+};
 
 
 // minify js
@@ -46,17 +51,22 @@ function jsmin() {
 
 //watchtask
 function watchTask() {
+    watch('*.html', copyHtml);
     watch('src/scss/**/*.scss', compilescss);
     watch('src/js/*.js', jsmin);
-    // watch('src/images/*', optimizeimg);
-    // watch('dist/images/*.{jpg,png}', webpImage);
+    watch('src/images/*', optimizeimg);
+    watch('dist/images/*.{jpg,png}', webpImage);
+
 }
 
 
 // Default Gulp task  
 //if we just typed gulp we well run all the functions in order
 exports.default = series(
+    copyHtml,
     compilescss,
+    optimizeimg,
+    webpImage,
     jsmin,
     watchTask
 );
